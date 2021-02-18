da3e810794646e646b184797877ae223627087fa
jaesbit <djtenzing@gmail.com>
2021-02-18 17:08:20 +0100

    Removed old_args added eslint Rewrited commander options descriptions Added support to proxyhosts

--- args.js
+++ args.js
@@ -1,18 +1,16 @@
-const program = require("commander");
-
+const program = require('commander')
 
 program
-    .option("--timeout <ms>", "wait in http request", 10000)
-    .option("--interval <ms>", "time to wait http pool for nex request", 50)
-    .option("--threads <Number>", "Pool size for simultaneous requests", 30)
-    .option("--proxyfiles [files...]", "files wich contain proxy hosts")
-    .option("--proxyuser <user>", "user to autenticate in proxy")
-    .option("--proxypass <password>", "password for autenticate in proxy")
-    .option("--nocert", "if set allows self certificate", false)
-    .option("--debugproxy", "if set write verbose information", false)
+  .option('--timeout <ms>', 'maximum waiting time on requests', 10000)
+  .option('--interval <ms>', 'waiting time to check the pool', 50)
+  .option('--threads <Number>', 'Number of parallel requests', 30)
+  .option('--proxyfiles [files...]', 'files with proxy hosts')
+  .option('--proxyhosts [hosts...]', 'list of proxy hostnames')
+  .option('--proxyuser <user>', 'user to authenticate the proxy')
+  .option('--proxypass <password>', 'password to authenticate the proxy')
+  .option('--nocert', 'if set, will not check the certificate', false)
+  .option('--debugproxy', 'prints verbose info', false)
 
+program.parse(process.argv)
 
-program.parse(process.argv);
-
-module.exports = program;
-// module.exports.proxyArgs = proxyOptions;
+module.exports = program
--- http.js
+++ http.js
@@ -1,6 +1,6 @@
-const axios = require("axios");
-const fs = require('fs');
-var SocksProxyAgent = require('socks-proxy-agent');
+const axios = require('axios')
+const fs = require('fs')
+const SocksProxyAgent = require('socks-proxy-agent')
 
 /**
  * Generate random number between low and high
@@ -8,76 +8,76 @@ var SocksProxyAgent = require('socks-proxy-agent');
  * @param {Number} high High range to generate random value
  * @returns {Number} Random number generated
  */
-function random(low, high) {
-    return Math.floor(Math.random() * (high - low + 1) + low)
+function random (low, high) {
+  return Math.floor(Math.random() * (high - low + 1) + low)
 }
 
 /**
  * Using proxy object will selected a random free proxy
- * 
+ *
  * @returns {SocksProxyAgent} Selected and configured proxy
  */
-function selectAgent() {
-    if (proxy.active) {
-        const index = random(0, proxy.available.length - 1);
-        const agent = proxy.available[index];
-        proxy.available.splice(index, 1);
-        proxy.working.push(agent);
-        if (options.debugproxy && agent == undefined) {
-            // I think is already fixed and will not raise this message any more
-            console.error(`failed to switch proxy, availables ${proxy.available.length}, espected: ${index}, in use: ${proxy.working.length}`);
-        }
-        return agent;
+function selectAgent () {
+  if (proxy.active) {
+    const index = random(0, proxy.available.length - 1)
+    const agent = proxy.available[index]
+    proxy.available.splice(index, 1)
+    proxy.working.push(agent)
+    if (options.debugproxy && agent == undefined) {
+      // I think is already fixed and will not raise this message any more
+      console.error(`failed to switch proxy, availables ${proxy.available.length}, espected: ${index}, in use: ${proxy.working.length}`)
     }
+    return agent
+  }
 }
 
 /**
  * Enables to reuse given agent
  * @param {SocksProxyAgent} agent Speficy an agent to mark as usable again to be available on selectAgent()
  */
-function unselectAgent(agent) {
-    if (proxy.active) {
-        const index = proxy.working.indexOf(agent);
-        if (index >= 0) { proxy.working.splice(index, 1); }
-        proxy.available.push(agent);
-    }
+function unselectAgent (agent) {
+  if (proxy.active) {
+    const index = proxy.working.indexOf(agent)
+    if (index >= 0) { proxy.working.splice(index, 1) }
+    proxy.available.push(agent)
+  }
 }
 
 /**
  * Interceptor for axios request
  * Before execute request will active any available proxy if it`s configured
  */
-function requestInterceptor(config) {
-    return new Promise((resolve, reject) => {
-        let interval = setInterval(() => {
-            if (PENDING_REQUESTS < options.threads && (!proxy.active || proxy.available.length > 0)) {
-                PENDING_REQUESTS++
-                clearInterval(interval);
-                const agent = selectAgent();
-                if (agent) {
-                    if (options.debugproxy) {
-                        console.log(`Switched to agent ${agent.proxy.host}`);
-                    }
-                    config.httpsAgent = agent;
-                    config.httpAgent = agent;
-                }
-                else if (proxy.active) {
-                    console.warn("switch agent but undefinded found!");
-                }
-                resolve(config);
-            }
-        }, options.interval);
-    })
+function requestInterceptor (config) {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (PENDING_REQUESTS < options.threads && (!proxy.active || proxy.available.length > 0)) {
+        PENDING_REQUESTS++
+        clearInterval(interval)
+        const agent = selectAgent()
+        if (agent) {
+          if (options.debugproxy) {
+            console.log(`Switched to agent ${agent.proxy.host}`)
+          }
+          config.httpsAgent = agent
+          config.httpAgent = agent
+        } else if (proxy.active) {
+          console.warn('switch agent but undefinded found!')
+        }
+        global.executedRequests++
+        resolve(config)
+      }
+    }, options.interval)
+  })
 };
 
 /**
  * Axios Response Interceptor when it succeded
- * Will unselect proxy agent if enabled 
+ * Will unselect proxy agent if enabled
  */
-function responseSuccessInterceptor(response) {
-    PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1);
-    unselectAgent(response.config.httpsAgent);
-    return Promise.resolve(response);
+function responseSuccessInterceptor (response) {
+  PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1)
+  unselectAgent(response.config.httpsAgent)
+  return Promise.resolve(response)
 }
 
 /**
@@ -86,155 +86,166 @@ function responseSuccessInterceptor(response) {
  *  If reponse available will return response as usual reponse
  *  Other cases will raise exception
  */
-function responseErrorInterceptor(error) {
-    PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1);
-    unselectAgent(error.config.httpsAgent);
-    // if (error.message.indexOf("timeout") >= 0) {
-    //     error.config.timeout = error.config.timeout * 2;
-    //     if (options.debugproxy) { console.log(`increased timeout to ${error.config.timeout}`); }
-    //     return axios.request(error.config);
-    // } else {
-    return new Promise((res, rej) => {
-        if (!error.response) {
-            rej(error);
-        } else {
-            res(error.response);
-        }
-    })
-    // }
+function responseErrorInterceptor (error) {
+  PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1)
+  unselectAgent(error.config.httpsAgent)
+  return new Promise((resolve, reject) => {
+    if (!error.response) {
+      reject(error)
+    } else {
+      resolve(error.response)
+    }
+  })
 }
 
 /**
  * This override Axios.create() by include interceptors defined above
- * 
+ *
  * @param {*} options Options like axios.create(options)
- * 
+ *
  */
-function create(options) {
-    const newClient = this.instance(options);
-    newClient.interceptors.request.use(requestInterceptor);
-    newClient.interceptors.response.use(responseSuccessInterceptor, responseErrorInterceptor);
-    newClient.download = download;
-    return newClient;
+function create (options) {
+  const newClient = this.instance(options)
+  newClient.interceptors.request.use(requestInterceptor)
+  newClient.interceptors.response.use(responseSuccessInterceptor, responseErrorInterceptor)
+  newClient.download = download
+  return newClient
 }
 
 /**
  * Defines or reconfigure proxy configuration
- * 
- * @param {Number} timeout Max timeout in ms to wait for response
- * @param {Number} interval Time where re-run tasks in ms
- * @param {Number} threads Amount of concurrent request 
- * @param {Array} proxyfiles String[] with paths where read line by line socks hostnames
- * @param {String} proxyuser User to autenticate proxy
- * @param {String} proxypass Password to autenticate proxy
- * @param {Boolean} nocert Default false, if true will ignore bad certs
- * @param {Boolean} debugproxy Enables verbose mode for proxy messages
+ *
+ * @param {Object} config
+ * @param {Number} config.timeout Max timeout in ms to wait for response
+ * @param {Number} config.interval Time where re-run tasks in ms
+ * @param {Number} config.threads Amount of concurrent request
+ * @param {Array} config.proxyfiles String[] with paths where read line by line socks hostnames
+ * @param {Array} config.proxyhosts String[] with proxy hostnames
+ * @param {String} config.proxyuser User to autenticate proxy
+ * @param {String} config.proxypass Password to autenticate proxy
+ * @param {Boolean} config.nocert Default false, if true will ignore bad certs
+ * @param {Boolean} config.debugproxy Enables verbose mode for proxy messages
  */
-function setup(timeout = 10000, interval = 50, threads = 30, proxyfiles = [], proxyuser = "", proxypass = "", nocert = false, debugproxy = false) {
-    options = {
-        timeout: timeout,
-        interval: interval,
-        threads: threads,
-        proxyfiles: proxyfiles,
-        proxyuser: proxyuser,
-        proxypass: proxypass,
-        nocert: nocert,
-        debugproxy: debugproxy,
-        proxyArgs: {}
-    }
-    initializeWithArgs();
+function setup ({
+  timeout,
+  interval,
+  threads,
+  proxyfiles,
+  proxyuser,
+  proxypass,
+  nocert,
+  debugproxy,
+  proxyhosts
+}) {
+  options = {
+    timeout: timeout || 10000,
+    interval: interval || 50,
+    threads: threads || 1,
+    proxyfiles: proxyfiles || [],
+    proxyhosts: proxyhosts || [],
+    proxyuser: proxyuser || '',
+    proxypass: proxypass || '',
+    nocert: nocert || false,
+    debugproxy: debugproxy || false,
+    proxyArgs: {}
+  }
+  initializeWithArgs()
 }
 
 /**
- * Initialize proxy configuration or not, based on self.options, 
+ * Initialize proxy configuration or not, based on self.options,
  */
-function initializeWithArgs() {
-    /**
+function initializeWithArgs () {
+  /**
      * When proxyfiles args is given will update proxy object to configure as socks5 proxies
      */
-    if (options.proxyfiles) {
-        options.proxyfiles.forEach(proxyFile => {
-            if (fs.existsSync(proxyFile)) {
-                String(fs.readFileSync(proxyFile)).split("\n").forEach(x => {
-                    if (x)
-                        proxy.available.push(new SocksProxyAgent({
-                            protocol: "socks5:",
-                            host: x,
-                            port: 1080,
-                            auth: `${options.proxyuser}:${options.proxypass}`,
-                            rejectUnauthorized: !options.nocert
-                        }));
-                })
-            }
-        });
-        options.threads = proxy.available.length - 1;
-        proxy.active = true;
+  options.proxyfiles.forEach(proxyFile => {
+    if (fs.existsSync(proxyFile)) {
+      String(fs.readFileSync(proxyFile)).split('\n').forEach(x => {
+        if (x) {
+          pushProxy(x)
+        }
+      })
     }
+  })
+
+  options.proxyhosts.forEach(x => {
+    pushProxy(x)
+  })
+
+  if (options.threads === 1 && proxy.available.length) {
+    options.threads = proxy.available.length - 1
+    proxy.active = true
+  }
+
+  function pushProxy (x) {
+    proxy.available.push(new SocksProxyAgent({
+      protocol: 'socks5:',
+      host: x,
+      port: 1080,
+      auth: `${options.proxyuser}:${options.proxypass}`,
+      rejectUnauthorized: !options.nocert
+    }))
+  }
 }
 
 /**
  * Download given url into file path
- * 
+ *
  * @param {String} url Url to download into file
  * @param {String} path Path to write Url file
  */
-async function download(url, path) {
-    const writer = fs.createWriteStream(path)
+async function download (url, path) {
+  const writer = fs.createWriteStream(path)
 
-    const response = await axios({
-        url,
-        method: 'GET',
-        responseType: 'stream'
-    })
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  })
 
-    response.data.pipe(writer)
+  response.data.pipe(writer)
 
-    return new Promise((resolve, reject) => {
-        writer.on('finish', resolve)
-        writer.on('error', reject)
-    })
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve)
+    writer.on('error', reject)
+  })
 }
 
 // Add axios default interceptors
-axios.interceptors.request.use(requestInterceptor);
-axios.interceptors.response.use(responseSuccessInterceptor, responseErrorInterceptor);
-axios.download = download;
-axios.instance = axios.create;
+axios.interceptors.request.use(requestInterceptor)
+axios.interceptors.response.use(responseSuccessInterceptor, responseErrorInterceptor)
+axios.download = download
+axios.instance = axios.create
+
+global.executedRequests = 0
 
 let PENDING_REQUESTS = 0
-let proxy = {
-    available: [],
-    working: [],
-    active: false
+const proxy = {
+  available: [],
+  working: [],
+  active: false
 }
 let options = {
-    timeout: 10000,
-    interval: 50,
-    threads: 30,
-    nocert: false,
-    debugproxy: false
-};
-
-try {
-    // If command-line-args is available will setup proxy with args
-    const _ = require.resolve("command-line-args");
-    options = require("./old_args");
-    console.warn("@command-line-args is depcreated now works with @commander")
-    initializeWithArgs();
-} catch (error) {
+  timeout: 10000,
+  interval: 50,
+  threads: 1,
+  nocert: false,
+  debugproxy: false
 }
 
-const { version } = require("./package");
+const { version } = require('./package')
 /**
  * Basically the module gives all functionallity of an axios instance with interceptors
  * Or gives you way to create your custom instance by calling self.create()
  */
-module.exports = axios;
-module.exports.create = create;
-module.exports.options = options;
-module.exports.setup = setup;
+module.exports = axios
+module.exports.create = create
+module.exports.options = options
+module.exports.setup = setup
+module.exports.counter = () => { return global.executedRequests }
 module.exports.enableCommander = () => {
-    options = require("./args");
-    initializeWithArgs();
+  options = require('./args')
+  initializeWithArgs()
 }
-module.exports.version = version;
+module.exports.version = version
